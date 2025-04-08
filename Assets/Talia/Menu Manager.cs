36a9e70c274231a365cc4668588312e15be49168
pegasiperspectives <113609176+pegasiperspectives@users.noreply.github.com>
2025-04-08 10:48:50 -0400

    added start menu/scene

---
+++ Assets/Talia/Menu Manager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void BeginGame()
+    {
+        SceneManager.LoadScene("TaliaTesting");
+    }
+}
