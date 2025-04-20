b8f21475181791af5fd4251d0cace031aa47328d
WWiLD-FiRE <17tannerman@gmail.com>
2025-04-20 14:25:06 -0400

    connecting script
    connecting script

--- Assets/Talia/Dialogue Manager.cs
+++ Assets/Talia/Dialogue Manager.cs
@@ -4,9 +4,17 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueUI : MonoBehaviour
 {
+
+
+    //Tanner Addition
+    public Transform itemButtonContainer;  
+    public GameObject itemButtonPrefab;
+    //added
+
 
     [SerializeField] public TMP_Text textLabel;
     [SerializeField] private float typeSpeed = 50;
@@ -39,21 +47,27 @@ public class DialogueUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.I))
+        if (Input.GetKeyDown(KeyCode.I) && self.activeInHierarchy == false) //added self chech so multiple objects arent made
         {
             self.SetActive(true);
             SetDialogueText(allDialogue[0], textLabel);
             Debug.Log("triggered dialogue box");
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
+
+            //tanner addition
+            ShowInventoryItemButtons();
+            //added
         }
-        else if (Input.GetKey(KeyCode.X))
+        else if (Input.GetKeyDown(KeyCode.X))
         {
             closeDialogue();
             Debug.Log("exited dialogue box");
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
+
+        
     }
 
     public void SetDialogueText(string textToType, TMP_Text textLabel)
@@ -83,17 +97,71 @@ public class DialogueUI : MonoBehaviour
     private void closeDialogue()
     {
         self.SetActive(false);
+
+        //tanner addition
+        ClearItemButtons();
+        //added
     }
 
-    public void Option1() {
+    public void Option1()
+    {
         SetDialogueText(allDialogue[1], textLabel);
     }
 
-    public void Option2() {
+    public void Option2()
+    {
         SetDialogueText(allDialogue[2], textLabel);
     }
 
-    public void Option3() {
+    public void Option3()
+    {
         SetDialogueText(allDialogue[3], textLabel);
     }
+
+
+
+
+
+    //Tanner addition
+    public void ShowInventoryItemButtons()
+    {
+       
+        foreach (var item in InventoryManager.Instance.Items)
+        {
+            GameObject button = Instantiate(itemButtonPrefab, itemButtonContainer);
+            var label = button.GetComponentInChildren<TMP_Text>();
+            label.text = "Show " + item.itemName;
+
+            button.GetComponent<Button>().onClick.AddListener(() => OnItemShown(item));
+        }
+    }
+
+    private void OnItemShown(Item item)
+    {
+        // Call the dialogue options here for the name of the object
+        if (item.itemName.Contains("Example1"))
+            SetDialogueText(allDialogue[1], textLabel);
+        else if (item.itemName.Contains("Example2"))
+            SetDialogueText(allDialogue[5], textLabel);
+        else
+            SetDialogueText(allDialogue[3], textLabel);
+    }
+
+    private void ClearItemButtons()// dont want repeated objects in inventory
+    {
+        foreach (Transform child in itemButtonContainer)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+    //added
+
+
 }
+
+
+
+
+
+
+
