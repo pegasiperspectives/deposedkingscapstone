24574adf2e775fb2cccbc07b949956af67d1c81c
WWiLD-FiRE <17tannerman@gmail.com>
2025-04-26 19:30:01 -0400

    remove sprint and increase gravity
    remove sprint and increase gravity

--- Assets/Trent/Scripts/FpsController.cs
+++ Assets/Trent/Scripts/FpsController.cs
@@ -9,7 +9,7 @@ public class FPSController : MonoBehaviour
     public float walkSpeed = 6f;
     public float runSpeed = 12f;
     public float jumpPower = 7f;
-    public float gravity = 10f;
+    public float gravity = 25f;
 
 
     public float lookSpeed = 2f;
@@ -47,8 +47,8 @@ public class FPSController : MonoBehaviour
 
         // Press Left Shift to run
         bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+        float curSpeedX = canMove ? (walkSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (walkSpeed) * Input.GetAxis("Horizontal") : 0;
         float movementDirectionY = moveDirection.y;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
