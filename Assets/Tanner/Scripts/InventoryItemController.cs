be42ff7a5599049b0fd62bbc8daf97076c8d862a
pegasiperspectives <113609176+pegasiperspectives@users.noreply.github.com>
2025-11-27 15:16:26 -0500

    trying to debug ugh

--- Assets/Tanner/Scripts/InventoryItemController.cs
+++ Assets/Tanner/Scripts/InventoryItemController.cs
@@ -59,11 +59,11 @@ public class InventoryItemController : MonoBehaviour
     void Update()
     {
         // If left mouse button is held down while inspecting, rotate object
-       // if (Input.GetMouseButton(0))
-        //{
+        if (Input.GetMouseButton(0))
+        {
             InspectingWithMouse();
             //Debug.Log("it should be calling inspecting with mouse rn");
-       // }
+        }
 
         // If pressing Tab while inspecting, exit inspection mode
         if (Input.GetKeyDown(KeyCode.Tab) && InventoryManager.currentlyInspecting == true)
@@ -280,7 +280,7 @@ public class InventoryItemController : MonoBehaviour
         }
         else
         {
-            currentObservable = InventoryManager.Instance.ObservableObject1;
+            currentObservable = null;
         }
 
     }
@@ -289,20 +289,29 @@ public class InventoryItemController : MonoBehaviour
     // Rotate the inspected object based on mouse movement
     public void InspectingWithMouse()
     {
+        if (currentObservable == null)
+        {
+            Debug.LogError("currentObservable IS NULL on object: " + gameObject.name);
+        }
+        else
+        {
+            Debug.Log("currentObservable IS VALID: " + currentObservable.name);
+        }
+
         Debug.Log("inspecting currently is" + InventoryManager.currentlyInspecting);
-        InventoryManager.currentlyInspecting = true;
+        //InventoryManager.currentlyInspecting = true;
 
         //Debug.Log("Inspectingwithmouse");
         if (InventoryManager.currentlyInspecting == true)
         {
             Debug.Log(sensitivity);
-            deltaRotationX = Input.GetAxisRaw("Mouse X") * sensitivity;           // Get mouse movement delta
-            deltaRotationY = Input.GetAxisRaw("Mouse Y") * sensitivity;
+            deltaRotationX = Input.GetAxis("Mouse X") * sensitivity;           // Get mouse movement delta
+            deltaRotationY = Input.GetAxis("Mouse Y") * sensitivity;
 
 
             Debug.Log("registering inspect rotation and rotation x and y are " + deltaRotationX.ToString() + " " + deltaRotationY.ToString());
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
+            //Cursor.lockState = CursorLockMode.None;
+            //Cursor.visible = true;
 
             if (deltaRotationX != 0 || deltaRotationY != 0)
             {
